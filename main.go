b97aff94e6e44ce8b8563bc3b15094d4ff3e04a6
Dmitry Ledentsov <dmlled@yahoo.com>
2023-03-13 22:39:49 +0100

    Revert "version feedback"
    This reverts commit e1951097f4cd620e41fc68e87db931288bc15cb9.

--- main.go
+++ main.go
@@ -5,7 +5,6 @@ import (
 	"math"
 	"os"
 	"path/filepath"
-	"runtime/debug"
 	"strings"
 
 	"github.com/charmbracelet/bubbles/paginator"
@@ -143,8 +142,6 @@ func (m model) View() string {
 
 	m.renderTable(&b, paths)
 
-	b.WriteString("\n" + version + "\n")
-
 	return b.String()
 }
 
@@ -187,14 +184,3 @@ func failWith(message string) {
 	fmt.Println(message)
 	os.Exit(1)
 }
-
-var version = func() string {
-	if info, ok := debug.ReadBuildInfo(); ok {
-		for _, setting := range info.Settings {
-			if setting.Key == "vcs.revision" {
-				return setting.Value
-			}
-		}
-	}
-	return "<unknown version>"
-}()
