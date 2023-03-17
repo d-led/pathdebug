bf1b058744183c609bb90fffad151c5740605e38
Dmitry Ledentsov <dmlled@yahoo.com>
2023-03-17 20:59:24 +0100

    version command

---
+++ cmd/version.go
@@ -0,0 +1,20 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/d-led/pathdebug/common"
+	"github.com/spf13/cobra"
+)
+
+func init() {
+	rootCmd.AddCommand(versionCmd)
+}
+
+var versionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "Print the version",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println(common.Version)
+	},
+}
