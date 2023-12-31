9b4b1b111dddcd144904f65f8320793cab80c34b
Dmitry Ledentsov <dmlled@yahoo.com>
2023-12-31 16:08:52 +0100

    preparation for actual sources computation

--- common/nix_candidate_source.go
+++ common/nix_candidate_source.go
@@ -1,13 +1,23 @@
 package common
 
+import "runtime"
+
 type NixCandidateSource struct {
-	fs Filesystem
+	fs      Filesystem
+	sources map[string]*PathSetIn
 }
 
 func NewNixCandidateSource(fs Filesystem) CandidateSource {
-	return &NixCandidateSource{fs}
+	return &NixCandidateSource{
+		fs,
+		map[string]*PathSetIn{},
+	}
 }
 
 func (s *NixCandidateSource) WhereSet(somePath string) *PathSetIn {
-	return nil
+	if runtime.GOOS == "windows" {
+		return nil
+	}
+	normalizedPath := s.fs.GetAbsolutePath(somePath)
+	return s.sources[normalizedPath]
 }
